a05787a7821c46ef042e8889e6f43fd691609fc6
JPaulo0691 <joaopauloaacosta@gmail.com>
2024-10-09 10:40:10 -0300

    add a classe JwtUserDetails

---
+++ src/main/java/org/example/idealstore/jwt/JwtUserDetails.java
@@ -0,0 +1,25 @@
+package org.example.idealstore.jwt;
+
+import org.example.idealstore.entity.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class JwtUserDetails extends User {
+
+    private Usuario usuario;
+
+    public JwtUserDetails(Usuario usuario) {
+        super(usuario.getUsername(), usuario.getPassword(), AuthorityUtils.createAuthorityList(usuario.getRole().name()));
+    }
+
+    public Long getId(){
+        return this.usuario.getId();
+    }
+
+    public String getRole(){
+        return this.usuario.getRole().name();
+    }
+}
