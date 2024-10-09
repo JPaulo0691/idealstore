a9ae3c5ce5d1db2b4092d8e76ce25c690b0ac2b5
JPaulo0691 <joaopauloaacosta@gmail.com>
2024-10-08 22:59:49 -0300

    criando a classe JWT Utils

---
+++ src/main/java/org/example/idealstore/jwt/utils/JwtUtils.java
@@ -0,0 +1,98 @@
+package org.example.idealstore.jwt.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.example.idealstore.jwt.JwtToken;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Slf4j
+public class JwtUtils {
+
+    public static final String JWT_BEARER = "Bearer ";
+    public static final String JWT_AUTHORIZATION = "Authorization";
+    public static final String SECRET_KEY = "0123456789-0123456789-0123456789";
+    public static final long EXPIRED_DAYS = 0;
+    public static final long EXPIRED_HOURS = 0;
+    public static final long EXPIRED_MINUTES = 2;
+
+    private JwtUtils(){
+
+    }
+
+    private static Key generateKey(){
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private static Date toExpireDate(Date start){
+        LocalDateTime dateTime = start.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        LocalDateTime endTime = dateTime.plusDays(EXPIRED_DAYS).plusHours(EXPIRED_HOURS).plusMinutes(EXPIRED_MINUTES);
+
+        return Date.from(endTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    private static JwtToken createToken(String username, String role){
+        Date issuedAt = new Date();
+        Date limit = toExpireDate(issuedAt);
+
+        String token = Jwts.builder()
+                .setHeaderParam("type", "JWT")
+                .setSubject(username)
+                .setIssuedAt(issuedAt)
+                .setExpiration(limit)
+                .signWith(generateKey(), SignatureAlgorithm.HS256)
+                .claim("role",role)
+                .compact();
+
+        return new JwtToken(token);
+    }
+
+    private static Claims getClaimsFromToken(String token) {
+
+        try{
+            return Jwts.parserBuilder()
+                    .setSigningKey(generateKey()).build()
+                    .parseClaimsJwt(refactorToken(token)).getBody();
+
+        }catch(JwtException ex){
+            log.error(String.format("Token inválido %s", ex.getMessage()));
+        }
+
+        return null;
+    }
+
+    private static String getUserName(String token){
+        return getClaimsFromToken(token).getSubject();
+    }
+
+    private static boolean isTokenValid(String token){
+
+        try{
+            Jwts.parserBuilder()
+                    .setSigningKey(generateKey()).build()
+                    .parseClaimsJws(refactorToken(token)).getBody(); //processar e validar um JWT assinado.
+
+            return true;
+        }catch(JwtException ex){
+            log.error(String.format("Token inválido %s", ex.getMessage()));
+        }
+        return false;
+    }
+
+    private static String refactorToken(String token) {
+        if(token.contains(JWT_BEARER)){
+            return token.substring(JWT_BEARER.length());
+        }
+
+        return token;
+    }
+
+}
